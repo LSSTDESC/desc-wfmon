178f5bb51a5ae6bea9b1b15f56d9e368244a9488
David Adams <dladams@bnl.gov>
2022-04-06 12:26:46 -0700

    Perfstat now reads mutiple files. 1.2.0
    And perstat nb is added.

--- desc/wfmon/perfstat.py
+++ desc/wfmon/perfstat.py
@@ -11,6 +11,7 @@ import os
 import glob
 import collections
 import pandas
+import desc.wfmon
 
 class PerfStatLogReader:
 
@@ -21,9 +22,11 @@ class PerfStatLogReader:
         """
         self.flglin = 'Performance counter stats for'
         self.dbg = dbg
-        self.nfile = 0
-        self.nval = 0
-        self.nmissing = 0
+        self.nfile = 0         # # of attempted log file reads
+        self.nkeep = 0         # # successful lof file reads
+        self.nmissing = 0      # # # file read missing data
+        self.nskip = 0         # # files not read or missing data
+        self.nval = 0          # # entries in the table
         self.dict = collections.OrderedDict()
         self._table = pandas.DataFrame()
 
@@ -36,11 +39,16 @@ class PerfStatLogReader:
             self._table = pandas.DataFrame(self.dict)
         return self._table
 
-    def read(self, fnam, indata={}, dbg=0):
+    def read_log(self, fnam, indata={}, dbg=0):
         """
         Read data from a file.
         The table is extended using the supplied map (indata) and the
-        per stat data found in the file.
+        perf stat data found in the file.
+        Arguments:
+          fnam - Input log file.
+          indata - map of index data to add to the table
+          dbg - 0 for silent
+        Returns 0 if successful
         """
         myname = "PerfStatLogReader:ctor"
         # build a log label from the input data.
@@ -122,6 +130,76 @@ class PerfStatLogReader:
                     print(f"{myname} {slab}: Padded {nadd} values metric {snam}.")
                 assert(len(vals) == nval)
             self.nval = nval
+            self.nkeep = self.nkeep + 1
+            return 0
         else:
             print(f"{myname} {slab}: No metrics found.")
             self.nmissing = self.nmissing + 1
+            self.nskip = self.nskip + 1
+            return 1
+
+
+    def read_mondb_logs(self, dbr, dbg=0):
+        """
+        Read data from logs referenced by a MondDbReader.
+        Each succesfully read log adds an entry to the perfstat table here.
+        The table is extended using the supplied map (indata) and the
+        per stat data found in the file.
+        Arguments:
+          dbr - MonDbReader
+          dbg - 0=silent
+        Return is a map:
+          nkeep - # files read succesfully
+          nskip - # files skipped
+        """
+        myname = 'PerfStatLogReader::read_mondb_logs'
+        if 'task' not in dbr.fixed:
+            if dbg:
+                print(f"{myname}: Fixing input task table.")
+            dbr.fix_tasks()
+        if 'times' not in dbr.fixed:
+            if dbg:
+                print(f"{myname}: Fixing input times.")
+            dbr.fix_times()
+        tsk = dbr.table('task')
+        ttr = dbr.table('try')
+        nkeep = 0
+        nskip = 0
+        for tid in range(len(tsk)):
+            indict = collections.OrderedDict()
+            for nam in ['run_idx', 'task_idx', 'task_id']: indict[nam] = tsk[nam][tid]
+            sqry = f"run_idx=={indict['run_idx']} and task_id=={indict['task_id']}"
+            # Find the matching row(s) in the try table and extract the run start and stop times.
+            ttrm = ttr.query(sqry)
+            nttrm = len(ttrm)
+            if len(ttrm) != 1:
+                print(f"{myname}: WARNING: Try count is {nttrm} for query '{sqry}' ")
+            indict['tstart'] = ttrm.task_try_time_running.iloc[0]
+            indict['tstop'] = ttrm.task_try_time_returned.iloc[0]
+            fnam = dbr.task_logs[tid]
+            if not os.path.exists(fnam):
+                if dbg >= 1:
+                    print(f"  File not found {fnam}")
+                nskip = nskip + 1
+            else:
+                if self.read_log(fnam, indict, max(dbg-1, 0)):
+                    if dbg >= 2:
+                        print(f"  Error reading {fnam}")
+                    nskip = nskip + 1
+                    self.nskip = self.nskip + 1
+                else:
+                    if dbg >= 2:
+                        print(f"  Read {fnam}")
+                    nkeep = nkeep + 1
+        if dbg >= 1:
+            print(f"{myname}: nskip = {nskip}")
+            print(f"{myname}: nkeep = {nkeep}")
+            print(f"{myname}: nfile = {self.nfile}")
+            print(f"{myname}:  nval = {self.nval}")
+            print(f"{myname}: nmiss = {self.nmissing}")
+            print(f"{myname}: Perf state names: {self.dict.keys()}")
+            n_cycles = 'cycles:u'
+            n_clock = 'task-clock:u'
+        tab = self.table()
+        tab['speed'] = tab[n_cycles]/tab[n_clock]
+        return {"nkeep":nkeep, "nskip":nskip}
