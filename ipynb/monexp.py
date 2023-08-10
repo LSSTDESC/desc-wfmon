37581c7ee841577ae8fb410f08fa7529a2373488
David Adams <dladams@bnl.gov>
2023-08-10 12:37:05 -0700

    Update monexp.py. 0.26.13.

--- ipynb/monexp.py
+++ ipynb/monexp.py
@@ -1,8 +1,11 @@
 class monexp:
+    tmin = 0
     tmax = 30
+    run_id = None
     taskrunmax = 105
     taskcount_interval = 1
     tzoff = 0*3600
     iosummax = 1200
     tlatencymax = 10
     tlatrunmax = 50
+
